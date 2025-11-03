8a89fd7e7cea47673ee6677291fcb01a4817199f
supermario9000 <lamporest@gmail.com>
2025-11-03 20:59:16 +0200

    project structure

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "src/headers/lib.hpp"
+
+int main() {
+    cout << "Hello, Blockchain!" << std::endl;
+    return 0;
+}
---
+++ src/headers/lib.hpp
@@ -0,0 +1,14 @@
+#ifndef LIB_HPP
+#define LIB_HPP
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+#endif // LIB_HPP
